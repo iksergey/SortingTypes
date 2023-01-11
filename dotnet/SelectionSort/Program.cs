874dd5323e87b8c0a69bf3eeb894b854082d1f2e
Sergei Kamianetskii <spam@ksergey.ru>
2023-01-11 20:54:37 +0300

    Организация структуры проекта

--- dotnet/SelectionSort/Program.cs
+++ dotnet/SelectionSort/Program.cs
@@ -1,21 +1,8 @@
-﻿void SortSelection(int[] collection)
-{
-  int size = collection.Length;
-  for (int i = 0; i < size - 1; i++)
-  {
-    int pos = i;
-    for (int j = i + 1; j < size; j++)
-    {
-      if (collection[j] < collection[pos]) pos = j;
-    }
-    int temp = collection[i];
-    collection[i] = collection[pos];
-    collection[pos] = temp;
-  }
-}
+﻿using static Sorting;
+using static Infrastructure;
 
-int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
-
-Console.WriteLine($"[{String.Join(',', array)}]");
+int[] array = CreateArray(10);
+Show(array);
 SortSelection(array);
-Console.WriteLine($"[{String.Join(',', array)}]");
+Show(array);
+
